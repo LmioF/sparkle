0cad5633652cbc9047471d2bfb4161cc2d2ffdcf
xishang0128 <xishang02@gmail.com>
2026-05-21 13:40:48 +0800

    fix(device): 修复设备路径解析仅 Windows 加载
    Sync from upstream xishang0128/sparkle@694e1bc

--- src/renderer/src/components/override/override-item.tsx
+++ src/renderer/src/components/override/override-item.tsx
@@ -185,11 +185,7 @@ const OverrideItem: React.FC<Props> = (props) => {
         />
       )}
       {showQrCode && info.url && (
-        <QRCodeModal
-          title={info.name}
-          url={info.url}
-          onClose={() => setShowQrCode(false)}
-        />
+        <QRCodeModal title={info.name} url={info.url} onClose={() => setShowQrCode(false)} />
       )}
       {confirmOpen && (
         <ConfirmModal
